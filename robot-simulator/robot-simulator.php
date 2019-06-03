b0e3a464606bcf272ced0877fd54d1fe62308f34
Philippa Wisniewski <pecfwisniewski@gmail.com>
2019-06-03 16:53:27 +0100

    Adding current solution for robot-simulator excluding malformed test

---
+++ robot-simulator/robot-simulator.php
@@ -0,0 +1,65 @@
+<?php
+
+class Robot {
+  const DIRECTION_NORTH = 'N';
+  const DIRECTION_SOUTH = 'S';
+  const DIRECTION_EAST = 'E';
+  const DIRECTION_WEST = 'W';
+
+  public $position;
+  public $direction;
+
+  function __construct($start, $direction) {
+    $this->position = $start;
+    $this->direction = $direction;
+  }
+
+  function turnRight() {
+    if ($this->direction == self::DIRECTION_NORTH) {
+      $this->direction = self::DIRECTION_EAST;
+    } elseif ($this->direction == self::DIRECTION_EAST) {
+      $this->direction = self::DIRECTION_SOUTH;
+    } elseif ($this->direction == self::DIRECTION_SOUTH) {
+      $this->direction = self::DIRECTION_WEST;
+    } else {
+      $this->direction = self::DIRECTION_NORTH;
+    }
+  }
+
+  function turnLeft() {
+    if ($this->direction == self::DIRECTION_NORTH) {
+      $this->direction = self::DIRECTION_WEST;
+    } elseif ($this->direction == self::DIRECTION_WEST) {
+      $this->direction = self::DIRECTION_SOUTH;
+    } elseif ($this->direction == self::DIRECTION_SOUTH) {
+      $this->direction = self::DIRECTION_EAST;
+    } else {
+      $this->direction =self::DIRECTION_NORTH;
+    }
+  }
+
+  function advance() {
+    if ($this->direction == self::DIRECTION_NORTH) {
+      $this->position[1]++;
+    } elseif ($this->direction == self::DIRECTION_SOUTH) {
+      $this->position[1]--;
+    } elseif ($this->direction == self::DIRECTION_EAST) {
+      $this->position[0]++;
+    } else {
+      $this->position[0]--;
+    }
+  }
+
+  function instructions($instructions) {
+    $instructionArray = str_split($instructions);
+    for($i = 0; $i < count($instructionArray); $i++) {
+      if($instructionArray[$i] == 'L') {
+        $this::turnLeft();
+      } elseif($instructionArray[$i] == 'A') {
+        $this::advance();
+      } elseif($instructionArray[$i] == 'R') {
+        $this::turnRight();
+      }
+    }
+  }
+}
